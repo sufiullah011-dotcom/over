4d9c4ec0601c73fe4eacf6d7e85c7a1a5b3100d3
sufiullah011 <sufi.ullah011@gmail.com>
2026-03-06 00:12:34 +0600

    added a readme file

--- over.py
+++ over.py
@@ -7,24 +7,26 @@ class test:
         return max(lists, key=get_total)
     def total(self,list):
         return sum(list)
-    def avarage(self,lis):
+    def average(self,lis):
         return sum(lis)/len(lis)
 g={}
 name=[]
 cat=test()
-while True:
-    print("\n                        CHOOSE                                   \n")
-    print("1)enter mark for student")
-    print("2)know the highest mark")
-    print("3)know the average for a student")
-    print("4)know the total for a student")
-    print("5)know how many students in system")
-    print("type exit if u are done")
-    a=input("enter your choice: ")
-    if a=="1":
-        d=input("enter name: ")
-        if d in g:
-            again=input("do u wanna replace the marks? ")
+if __name__=="__main__":
+     while True:
+      print("\n                        CHOOSE                                   \n")
+      print("1)enter mark for student")
+      print("2)know the highest mark")
+      print("3)know the average for a student")
+      print("4)know the total for a student")
+      print("5)know how many students in system")
+      print("6)know the stored marks for a student")
+      print("type exit if u are done")
+      a=input("enter your choice: ")
+      if a=="1":
+         d=input("enter name: ")
+         if d in g:
+            again=input("do u wanna replace the marks? ").lower()
             if again=="no":
                 print("ok")
                 continue
@@ -36,55 +38,61 @@ while True:
                     mark1=(int(input("enter marks: ")))
                     if mark1>100 or mark1<0:
                         print("mark not acpectable")
+                        for l in range(2):
+                         g.pop(d,mark)
                     else:
                         mark.append(mark1)
                         g[d]=mark
-            print("student suscessfully added")
+                        print("student suscessfully added")
             print(g)
-        if d not in g:
+         if d not in g:
             mark=[]
             name.append(d)
             for i in range(2):
                 mark1=(int(input("enter marks: ")))
                 if mark1>100 or mark1<0:
                  print("mark not acpectable")
+                 g.pop(d,mark)
                 else:
                     mark.append(mark1)
                     g[d]=mark
-            print("student suscessfully added")
+                    print("student suscessfully added")
             print(g)
 
 
-    elif a=="2":
-        if g:
-            hi = cat.highest(g)
-            print("the highest total was achieved by", hi)
-        else:
-            print("no data")
-    elif a=="3":
-        average1=input("enter name of the student: ")
-        if average1 in g:
-            hello=cat.avarage(g[average1])
-            print(f"{average1}'s average is {hello}")
-    elif a=="4":
+         elif a=="2":
+                if g:
+                  hi = cat.highest(g)
+                  print("the highest total was achieved by", hi)
+                else:
+                 print("no data")
+         elif a=="3":
+           average1=input("enter name of the student: ")
+           if average1 in g:
+             hello=cat.average(g[average1])
+             print(f"{average1}'s average is {hello}")
+      elif a=="4":
         total1=input("enter the name: ")
         if total1 in g:
            bag= cat.total(g[total1])
            print(f"the total for {total1} is {bag}")
         else:
             print("name not in system")
-    elif a=="exit":
+      elif a=="exit":
         print("thanks for visting !")
         break
-    elif a=="5":
+      elif a=="5":
        if g:    
         print(f'number of students in system is {len(name)}')
        else:
            print("no data")
-if __name__=="__main__":
-    print(cat.total([2,2]))
-    print(cat.avarage([10,2]))
-    print(cat.highest([10,2],[100,1]))
+      elif a=="6":
+         know=input("enter the name of the student: ")
+         if know in g:
+            print(g[know])
+      else:
+       print("invalid choice please enter the number of your choice")
+
 
 
 
--- test.py
+++ test.py
@@ -1,14 +1,20 @@
-import over
 import unittest
+import over
+
 class TestTotal(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = over.test()
+
     def test_total(self):
-     self.assertEqual(over.total([10,2]), 12)
-
-    def test_highest(self):
-        self.assertEqual(
-            over.highest([[100,1],[1,11]]),[100,1])
-
+        self.assertEqual(self.obj.total([10,2]), 12)
+    def test_high(self):
+        self.assertEqual(self.obj.highest({
+            "azmina": [22, 33],
+            "azwa": [22, 23]}),"azmina")
     def test_average(self):
-        self.assertEqual(over.average([2,2]), 2)
-if __name__=="__main__":
+        self.assertEqual(self.obj.average([2,2]), 2)
+
+
+if __name__ == "__main__":
     unittest.main()
